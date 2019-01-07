54c53917ffe5962d6ee5b71e391d0756c8f49fa0
Rob Majuri <robmajuri@gmail.com>
2019-01-07 17:02:51 -0500

    feat: created category and product models

---
+++ server/db/models/product.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://i.pinimg.com/originals/2d/94/33/2d94334b737efb5d3a5ef32aef9daefc.jpg'
+  }
+})
+
+module.exports = Product
