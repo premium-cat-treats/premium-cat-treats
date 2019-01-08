10dd51a1b383e27ca52d7fb24c4b9c0640e91bec
Robert Majuri <38230125+rmajuri@users.noreply.github.com>
2019-01-08 14:44:30 -0500

    Feat: Navbar  (#13)
    * Fix: Update models according to Gabe's suggestions.
    
    * Add most of what is needed for product reducers
    
    * #12-navbar and basic design
    
    * navbar
    
    * Update navbar.js
    
    * Update auth-form.js
    
    * Update style.css

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
+  <div className="navbar">
+    <h3>PREMIUM CAT TREATS</h3>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -24,7 +24,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         </div>
       )}
     </nav>
-    <hr />
   </div>
 )
 
