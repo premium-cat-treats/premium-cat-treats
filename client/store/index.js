9417ec6a0e41ee8369819354be83669b7910190e
Rob Majuri <robmajuri@gmail.com>
2019-01-08 12:20:16 -0500

    navbar

--- client/store/index.js
+++ client/store/index.js
@@ -4,7 +4,9 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
