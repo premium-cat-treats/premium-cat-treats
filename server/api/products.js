031fbfe5640461a5df8096fe275ac2be64b47725
Sarah Buckingham <38636887+sbuckingham89@users.noreply.github.com>
2019-01-08 10:11:10 -0800

    created routes for products and category, added eager loading to db m… (#14)
    * created routes for products and category, added eager loading to db model
    
    * changed module names for consistency, changed class method name on db, edited put and delete routes
    
    closes #9

---
+++ server/api/products.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const Product = require('../db/models/product')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.send(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const specificProduct = await Product.findById(req.params.id)
+    res.send(specificProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//TODO: add logic where you must be an admin
+router.post('/', async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body)
+    res.status(201).send(newProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    await Product.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    const updatedProduct = await Product.findById(req.params.id)
+    res.status(201).send(updatedProduct)
+  } catch (error) {
+    next(error)
+  }
+})
