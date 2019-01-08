dfa1476ba527d6558ed16f5a9d9c6cb346b9a6dd
Neil Scheuermann <neilscheuermann@gmail.com>
2019-01-07 17:32:45 -0700

    Created an AllProducts component and added to client/app.js

---
+++ client/components/AllProducts.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class AllProducts extends Component {
+  componentDidMount() {
+    // Uncomment line below when we have a thunk imported from the store named fetchProducts.
+    // this.props.fetchProducts();
+  }
+
+  render() {
+    const {products} = this.props
+
+    return (
+      <div>
+        {products ? (
+          <div>Will map over all the products here.</div>
+        ) : (
+          <div>No products in database.</div>
+        )}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  products: state.products
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
