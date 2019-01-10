a3d0f2d8252e84daf10063a33e537388dfc3f8c2
Neil Scheuermann <neilscheuermann@gmail.com>
2019-01-10 09:41:06 -0700

    Added drop-ddown and 'add to cart' button

--- client/components/Product.js
+++ client/components/Product.js
@@ -8,16 +8,37 @@ class Product extends Component {
     this.props.getUser()
   }
 
+  handleSubmit = event => {
+    const {id, title} = this.props.product
+    event.preventDefault()
+    const quantity = document.getElementById(`${id}-quantity-drop-down`)
+    console.log(quantity.value, title)
+  }
+
   render() {
-    const {product} = this.props
+    const {title, priceCents, quantity, description, id} = this.props.product
 
     return (
       <div>
-        <h3>{product.title}</h3>
-        <p>Price: ${product.priceCents / 100}</p>
-        <p>Description: {product.description}</p>
+        <h3>{title}</h3>
+        <p>Price: ${priceCents / 100}</p>
+        <p>Quantity in Stock: {quantity}</p>
+        <p>Description: {description}</p>
+
+        {/* Drop-down to allow user to add to cart */}
+        <form onSubmit={this.handleSubmit}>
+          <select id={`${id}-quantity-drop-down`} defaultValue="1">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button type="submit">Add to Cart</button>
+        </form>
+
         {this.props.user.adminAccess ? (
-          <Link to={`/products/admin/${product.id}`}>Edit this Product</Link>
+          <Link to={`/products/admin/${id}`}>Edit this Product</Link>
         ) : null}
       </div>
     )
