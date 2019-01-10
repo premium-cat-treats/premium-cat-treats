acde4eeda87ce00d74c6262833569e47f49f987b
Neil Scheuermann <neilscheuermann@gmail.com>
2019-01-10 16:26:41 -0700

    Trying to de-bug eslint error

--- client/components/Product.js
+++ client/components/Product.js
@@ -2,21 +2,38 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {me} from '../store/user'
+import {addToCart} from '../store/cart'
 
 class Product extends Component {
   componentDidMount() {
     this.props.getUser()
   }
 
-  handleSubmit = event => {
-    const {id, title} = this.props.product
-    event.preventDefault()
-    const quantity = document.getElementById(`${id}-quantity-drop-down`)
-    console.log(quantity.value, title)
+  handleSubmit = product => {
+    const quantity = Number(this.refs[`${product.id}-quantity-drop-down`].value)
+    this.props.addToCart(product.id, quantity)
+  }
+
+  // Function to dynamically add numbered options to the
+  // drop-down menu, so we don't display more items than
+  // are in stock.
+  createOptions = x => {
+    let options = []
+
+    for (let i = 1; i <= x; i++) {
+      options.push(
+        <option key={i} value={`${i}`}>
+          {i}
+        </option>
+      )
+    }
+
+    return options
   }
 
   render() {
     const {title, priceCents, quantity, description, id} = this.props.product
+    // console.log(this.createOptions(5))
 
     return (
       <div>
@@ -26,16 +43,20 @@ class Product extends Component {
         <p>Description: {description}</p>
 
         {/* Drop-down to allow user to add to cart */}
-        <form onSubmit={this.handleSubmit}>
-          <select id={`${id}-quantity-drop-down`} defaultValue="1">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+        <div>
+          <select ref={`${id}-quantity-drop-down`} defaultValue="1">
+            {this.createOptions(quantity)}
+            {/* <option key={1} value={1}>
+              {1}
+            </option> */}
           </select>
-          <button type="submit">Add to Cart</button>
-        </form>
+          <button
+            type="button"
+            onClick={() => this.handleSubmit(this.props.product)}
+          >
+            Add to Cart
+          </button>
+        </div>
 
         {this.props.user.adminAccess ? (
           <Link to={`/products/admin/${id}`}>Edit this Product</Link>
@@ -50,7 +71,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  getUser: () => dispatch(me())
+  getUser: () => dispatch(me()),
+  addToCart: (productId, quantity) => dispatch(addToCart(productId, quantity))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product)
--- client/store/cart.js
+++ client/store/cart.js
@@ -10,6 +10,17 @@ export const addToCart = (productId, quantity) => ({
 export const cartReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
+      // If the item already exists in the cart, add to
+      // the quantity already associated with the productId.
+      if (state.some(item => item.productId === action.payload.productId)) {
+        return state.map(item => {
+          if (item.productId === action.payload.productId) {
+            item.quantity = item.quantity + action.payload.quantity
+          }
+          return item
+        })
+      }
+
       return [...state, action.payload]
     default:
       return state
