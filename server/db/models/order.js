5c3b3442f30ea42324e12eef6e4f4da6957d8b91
Rob Majuri <robmajuri@gmail.com>
2019-01-10 21:23:58 -0500

    created associations for orders, users, products, order total

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,10 +4,22 @@ const db = require('../db')
 const Order = db.define('order', {
   status: {
     type: Sequelize.STRING,
+    defaultValue: 'Created',
     allowNull: false,
     validate: {
       isIn: [['Created', 'Processing', 'Canceled', 'Completed']]
     }
+  },
+  historicalPriceCents: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  quantityOrdered: {
+    type: Sequelize.INTEGER,
+    allowNull: false
   }
 })
 
