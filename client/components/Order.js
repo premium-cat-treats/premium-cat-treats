fc37c8d62003d2688331cd9ee74dde04bba1e4aa
Rob Majuri <robmajuri@gmail.com>
2019-01-12 20:18:35 -0500

    merge

--- client/components/Order.js
+++ client/components/Order.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
 const Order = props => {
-  const {orderItem, image, price, quantity, status} = props
+  const {productId, orderItem, image, price, quantity, status} = props
   return (
     <div className="single-order">
-      <h4>{orderItem}</h4>
-      <img src={image} />
-      <p>
+      <h3 style={{marginBottom: '5px'}}>{orderItem}</h3>
+      <p className="single-order-paragraph" style={{marginBottom: '15px'}}>
+        <strong>Product Id: </strong>
+        {productId}
+      </p>
+      <img src={image} style={{marginBottom: '15px'}} />
+      <p className="single-order-paragraph">
         <strong>Price: </strong>${price}
       </p>
-      <p>
+      <p className="single-order-paragraph">
         <strong>Qty: </strong>
         {quantity}
       </p>
-      <p>
+      <p className="single-order-paragraph">
         <strong>Status: </strong>
         {status}
       </p>
