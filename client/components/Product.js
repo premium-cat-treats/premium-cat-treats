5c14cb9945a8cd6ad119d7e236ef150199a19dc0
neilscheuermann <42921947+neilscheuermann@users.noreply.github.com>
2019-01-15 14:21:17 -0700

    Chore add dummy data to seed file (#85)
    * Added more pdata to seed file.
    
    * Made some style cahnges

--- client/components/Product.js
+++ client/components/Product.js
@@ -35,39 +35,43 @@ class Product extends Component {
       </option>
     ))
     return !product.deleted ? (
-      <Card>
-        <Image src={product.imageUrl} />
-        <Card.Content>
-          <Card.Header>{product.title}</Card.Header>
-          <Card.Meta>
-            <span className="price">${product.priceCents / 100}</span>
-          </Card.Meta>
-          <Card.Description>{product.description}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div>
-            <select ref={`${product.id}-quantity-drop-down`} defaultValue="1">
-              {options}
-            </select>
-            {/* Button disables if the user already has already placed all available items in stock into their cart. */}
-            <button
-              type="button"
-              disabled={product.quantity - cartItemQuantity ? '' : 'true'}
-              onClick={() => this.handleSubmit(product)}
-            >
-              Add to Cart
-            </button>
-          </div>
-          <div>
-            {this.props.user.adminAccess ? (
-              <Link to={`/products/admin/${product.id}`}>
-                <Icon name="edit" />
-                Edit this Product
-              </Link>
-            ) : null}
-          </div>
-        </Card.Content>
-      </Card>
+      <div className="product-card">
+        <Card>
+          <Image src={product.imageUrl} />
+          <Card.Content>
+            <Card.Header>{product.title}</Card.Header>
+            <Card.Meta>
+              <span className="price">
+                ${(product.priceCents / 100).toFixed(2)}
+              </span>
+            </Card.Meta>
+            <Card.Description>{product.description}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div>
+              <select ref={`${product.id}-quantity-drop-down`} defaultValue="1">
+                {options}
+              </select>
+              {/* Button disables if the user already has already placed all available items in stock into their cart. */}
+              <button
+                type="button"
+                disabled={product.quantity - cartItemQuantity ? '' : 'true'}
+                onClick={() => this.handleSubmit(product)}
+              >
+                Add to Cart
+              </button>
+            </div>
+            <div>
+              {this.props.user.adminAccess ? (
+                <Link to={`/products/admin/${product.id}`}>
+                  <Icon name="edit" />
+                  Edit this Product
+                </Link>
+              ) : null}
+            </div>
+          </Card.Content>
+        </Card>
+      </div>
     ) : null
   }
 }
