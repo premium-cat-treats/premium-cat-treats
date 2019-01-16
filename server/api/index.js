77a5dd8122153597ad8e0ce454f8859d96e34508
Robert Majuri <38230125+rmajuri@users.noreply.github.com>
2019-01-16 10:36:03 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -8,16 +8,10 @@ router.use('/orders', require('./orders'))
 router.use('/order', require('./order'))
 router.use('/order-total', require('./orderTotal'))
 
-function isAccessGranted(req, res, next) {
-  // Here your authorization logic (jwt, OAuth, custom connection logic...)
-  if (!isGranted) return res.status(401).end()
-  next()
-}
-
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
 })
 
-router.use('*', isAccessGranted)
+
