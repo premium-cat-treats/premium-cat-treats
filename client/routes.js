35b665c84da658501a2d502a10eccea21984c5ce
Neil Scheuermann <neilscheuermann@gmail.com>
2019-01-15 18:06:22 -0700

    Made isAdmin false by  default while fetching

--- client/routes.js
+++ client/routes.js
@@ -86,7 +86,7 @@ const mapState = state => {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     isLoggedIn: !!state.user.id,
-    isAdmin: state.user.adminAccess
+    isAdmin: state.user.adminAccess || false
   }
 }
 
